8f5d191d2593e9f8a19ab8a8e7dac6c4395f694c
X Wang <24860335+xwang233@users.noreply.github.com>
2020-04-26 22:59:23 -0700

    lint

--- pacman/main.cpp
+++ pacman/main.cpp
@@ -3,11 +3,11 @@
 #include "config.h"
 
 int main() {
-    printf("hello world\n");
+  printf("hello world\n");
 
-    std::cout << "versions " << std::endl
-              << "major " << VERSION_MAJOR << std::endl
-              << "minor " << VERSION_MINOR << std::endl;
+  std::cout << "versions " << std::endl
+            << "major " << VERSION_MAJOR << std::endl
+            << "minor " << VERSION_MINOR << std::endl;
 
-    return 0;
+  return 0;
 }
