9f15fac52214f4baf8ac2b91f2e334bc60253df1
X Wang <24860335+xwang233@users.noreply.github.com>
2020-05-07 02:50:15 -0700

    update example

--- pacman/main.cpp
+++ pacman/main.cpp
@@ -13,19 +13,23 @@ int main() {
             << "major " << VERSION_MAJOR << std::endl
             << "minor " << VERSION_MINOR << std::endl;
 
-  // pacman::utils::RNG::manual_seed(42);
+  pacman::utils::RNG::manual_seed(42);
 
   auto &rng = pacman::utils::RNG::get();
   std::cout << rng.frand() << std::endl;
 
-  auto map = pacman::map::Map(20, 20);
+  auto map = pacman::map::Map(40, 100);
+  // map.print();
+
+  auto pm = pacman::pacman::Pacman(map, pacman::pacman::MazeSolverAlgs::DFS);
+  pm.solve();
+  std::cout << map.path_count() << std::endl;
   map.print();
 
-  auto pm = pacman::pacman::Pacman(map);
-  pm.solve();
-
-  std::cout << std::endl;
-
+  auto pm1 =
+      pacman::pacman::Pacman(map, pacman::pacman::MazeSolverAlgs::Dijkstra);
+  pm1.solve();
+  std::cout << map.path_count() << std::endl;
   map.print();
 
   return 0;
